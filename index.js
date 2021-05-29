31e740995fbc036c2cdd6a566693d2f8ac47d506
Yesaswi Vadapalli <yesaswi.vadapalli@gmail.com>
2021-05-29 22:12:52 +0530

    Basic

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 const express = require('express')
 const dotenv = require("dotenv")
 
+// Dotenv Activate
+dotenv.config()
+
 // Express JS
 const app = express()
 
 // Routes
-const authRoute = require("./routes/auth")
-
-// Dotenv Activate
-dotenv.config()
-
+const helloRoute = require("./routes/hello")
+const userRoute = require("./routes/user")
 // Database Connection
 
 // Body Parser
@@ -18,9 +18,9 @@ app.use(express.json())
 
 // const port = 3000
 
-app.use("/api/user", authRoute);
+app.use("/api/hello", helloRoute);
 
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log(`App is running on http://localhost:${port}`)
+  console.log(`App is running on http://localhost:${process.env.PORT}`)
 })
